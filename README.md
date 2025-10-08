9376e4b653a92489bdb429f76f7561d49504f102
frontend200tb <frontend200tb@mail.ru>
2025-10-08 20:53:58 +0300

    feat:2025.10.08

--- README.md
+++ README.md
@@ -5,9 +5,8 @@ https://frontend200tb.github.io/200cf/dist/
 
 Сначала установим необходимые пакеты. Я их устанавливаю глобально, чтобы не появлялась лишняя папка node_modules в моем проекте
 
-npm i -g html-loader
+npm i -g html-loader html-webpack-plugin
 npm i -g css-loader mini-css-extract-plugin
 npm i -g sass-loader sass
 npm i -g postcss-loader postcss postcss-preset-env
-npm i -g html-webpack-plugin
 npm i -g babel-loader @babel/core @babel/preset-env
--- src/components/cf/sch19/js/data-pages.js
+++ src/components/cf/sch19/js/data-pages.js
@@ -7,6 +7,19 @@ import htmlSch5         from './../pages/elem-sch5.html';
 import htmlSch6         from './../pages/elem-sch6.html';
 import htmlSch7         from './../pages/elem-sch7.html';
 import htmlSch8         from './../pages/elem-sch8.html';
+import htmlSch9         from './../pages/elem-sch9.html';
+import htmlSch10        from './../pages/elem-sch10.html';
+import htmlSch11        from './../pages/elem-sch11.html';
+import htmlSch12        from './../pages/elem-sch12.html';
+import htmlSch13        from './../pages/elem-sch13.html';
+import htmlSch14        from './../pages/elem-sch14.html';
+import htmlSch15        from './../pages/elem-sch15.html';
+import htmlSch16        from './../pages/elem-sch16.html';
+import htmlSch17        from './../pages/elem-sch17.html';
+import htmlSch18        from './../pages/elem-sch18.html';
+import htmlSch19        from './../pages/elem-sch19.html';
+import htmlSch20        from './../pages/elem-sch20.html';
+import htmlSch21        from './../pages/elem-sch21.html';
 
 export const pages = [
   htmlSch1,
@@ -17,4 +30,17 @@ export const pages = [
   htmlSch6,
   htmlSch7,
   htmlSch8,
+  htmlSch9,
+  htmlSch10,
+  htmlSch11,
+  htmlSch12,
+  htmlSch13,
+  htmlSch14,
+  htmlSch15,
+  htmlSch16,
+  htmlSch17,
+  htmlSch18,
+  htmlSch19,
+  htmlSch20,
+  htmlSch21,
 ];
