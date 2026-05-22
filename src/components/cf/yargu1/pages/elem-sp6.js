0865035761242705a26d20793d0e6c4cbb08a635
frontend200tb <frontend200tb@mail.ru>
2026-05-22 20:56:35 +0300

    2026.05.22

--- src/components/cf/yargu1/pages/elem-sp6.js
+++ src/components/cf/yargu1/pages/elem-sp6.js
@@ -1,7 +1,11 @@
+import i    from '../../rounds/z/z100/z16b.html';
 import p    from '../../rounds/z/z134b.html';
 
 export default function inElem() {
 
+  if (document.querySelector('.t9')) {
+    document.querySelector('.t9').innerHTML = i;
+  }
   if (document.querySelector('.t16')) {
     document.querySelector('.t16').innerHTML = p;
   }
